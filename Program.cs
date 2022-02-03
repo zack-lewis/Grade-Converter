a914b8eedf312e40d79d6dbc10915318207fbfc4
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-02-02 20:03:58 -0600

    Building Functions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,211 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeConverter
 {
     class Program
     {
+        // GLOBAL VARIABLES
+        const double upperLimit = 110;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // VARIABLES
+            List<double> grades = new List<double>();
+            int numGrades = 0;
+            string name = "";
+
+            name = greetUser();
+
+            numGrades = getNumGrades();
+            
+            // loop that many times
+            Console.WriteLine("We can do that. Let's get them input into our system.");
+            for(int i = 0; i < numGrades; i++) {
+                double input = gradePrompt(i+1.ToString());
+                grades = addGradeToList(input, grades);
+            }
+
         }
+
+        // FUNCTIONS
+
+        // gradePrompt
+        // Summary: Prompt user for a grade and return it as a double
+        // Arguments: 
+        //      string prompt - Prompt to use for user input
+        // Returns: double grade - Grade from user 
+        private static double gradePrompt(string prompt) {
+            
+                // temp string for input
+                string temp = "";
+                double grade = 0;
+
+                // ask user for input
+                Console.Write($"{prompt}: ");
+                temp = Console.ReadLine();
+
+                try {
+                    // if empty, reprompt
+                    if (temp == "") {
+                        throw new ArgumentNullException();
+                    }
+                    
+                    // if non-numeric, throw exception
+                    grade = Int32.Parse(temp);
+
+                    // if outside of bounds, throw exception
+                    if (grade > upperLimit || grade < 0) {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                     
+                }
+                catch (Exception) {
+                    throw;
+                }
+
+                return grade;
+        }
+
+        // addGradeToList
+        // Summary: Take a grade and a list, add the grade to the list, return the list
+        // Arguments: 
+        //      double grade - grade to add to list
+        //      List<double> list - initial list in which to add grade
+        // Returns: List<double> list - List with grade added
+        private static List<double> addGradeToList(double grade, List<double> list) {
+            try {
+                list.Add(grade);
+            }
+            catch (Exception ex) {
+                throw;
+            }
+
+            return list;
+        }
+
+        // percentToLetterGrade
+        // Summary: Take Numeric grade (as a percentage) and return a Letter grade (whole letters only)
+        // Arguments:
+        //      double grade - Grade to convert from Percent to Letter
+        // Return: string letterGrade - Equivilent letter grade in string format
+        private static string percentToLetterGrade(double grade) {
+            string letterGrade = "";
+            if(grade <= 60) {
+                letterGrade = "F";
+            }
+            else if(grade <= 70) {
+                letterGrade = "D";
+            }
+            else if(grade <= 80) {
+                letterGrade = "C";
+            }
+            else if(grade <= 90) {
+                letterGrade = "B";
+            }
+            else if(grade <= 100) {
+                letterGrade = "A";
+            }
+            else if(grade > 100) {
+                letterGrade = "A+";
+            }
+            else {
+                letterGrade = "U"; // Return Unsatisfactory if all else fails
+            }
+
+            return letterGrade;
+        }
+        
+        // listGrades
+        // Summary: Take a list of grades and output in pretty format
+        // Arguments:
+        //      List<double> list - list of grades to display
+        // Returns: none
+        private static void listGrades(List<double> list) {
+            for(int i = 0; i < list.Count(); i++) {
+                Console.WriteLine($"{i+1}: {list[i]}");
+            }
+        }
+
+        // - show stats
+        // - error handling
+
+        // highestGrade
+        // Arguments:
+        //      List<double> list - list of grades to search
+        // Returns: double highest - Highest grade from list as a percentage 
+        private static double highestGrade(List<double> list) {
+            double highest = 0;
+            foreach(double g in list) {
+                if(g > highest) {
+                    highest = g;
+                }
+            }
+            return highest;
+        }
+        
+
+        // lowestGrade
+        // Arguments:
+        //      List<double> list - list of grades to search
+        // Returns: double lowest - Lowest grade from list as a percentage 
+        private static double lowestGrade(List<double> list) {
+            double lowest = 999;
+            foreach(double g in list) {
+                if(g < lowest) {
+                    lowest = g;
+                }
+            }
+            return lowest;
+        }
+
+        // greetUser
+        // Arguments:
+        //      none
+        // Returns: string name - Full name of user
+        private static string greetUser() {
+            string firstName;
+            string lastName;
+            string name;
+
+            // ask user name
+            Console.WriteLine("Welcome to the Mighty Grade Converter. May I get your first and last name?");
+            Console.Write("First Name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("\n\n");
+
+            // welcome user
+            Console.WriteLine($"Wonderful! Welcome, {firstName} {lastName}, to the Greated Converter of Grades this side of the Mississippi!");
+
+            name = $"{firstName} {lastName}";
+            return name;
+        }
+
+        // getNumGrades
+        // Arguments:
+        //      none
+        // Returns: int num - Number of grades user wants to input
+        private static int getNumGrades() {
+            int num = 0;
+            // ask user number of grades to input
+            Console.Write("How many grades do you have to convert this fine day? ");
+            string numGradesRead = Console.ReadLine();
+            try {
+                num = Int32.Parse(numGradesRead);
+            }
+            catch (Exception) {
+                throw;
+            }
+
+            return num;
+        }
+ 
+ 
+ 
+ 
+ 
+ 
     }
 }
